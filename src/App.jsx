4ec8cf22ba227227cb54a68fcb3e1b7538d68b67
Ytopioi <mar.al.savenkova@gmail.com>
2022-03-26 21:05:06 +0300

    Posts_site(v.1.3-mui)

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,35 @@
 import React, { useState } from "react";
-import styled from 'styled-components';
 import cn from "classnames";
-import { Layout, Menu, Breadcrumb} from "antd";
-import { Logo } from "./components/Logo";
 import { postData } from "./posts";
 import { PostsList } from "./components/PostsList";
 import Button from "./components/Button";
-const { Header, Content, Footer } = Layout;
-
-const Styles = styled.div`
-  .header, .footer, .menu {
-    background-color: rgb(153 109 241);
-    
-    &:hover {
-      color: white;
-    }
-  }
-
-  .footer {
-    padding-bottom: 40px;
-  }
-`
+import Header from "./components/Header";
+import Breadcrumb from "./components/Breadcrumbs";
+import Pagination from "./components/Pagination";
+import Footer from "./components/Footer";
+import { Container } from "@mui/material";
 
 export const App = () => {
+
   const [posts, setPosts] = useState(postData);
+  const [rows, setRows] = useState(3);
+  const [page, setPage] = useState(1);
 
   return (
-    <Styles>
-      <Layout>
-        <Header className="header" style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-          <div className="logo">
-            <Logo />
-          </div>
-
-          <Menu className="menu" theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-            <Menu.Item key="1">Home</Menu.Item>
-            <Menu.Item key="2">Remix Docs</Menu.Item>
-            <Menu.Item key="3">GitHub</Menu.Item>
-          </Menu>
-        </Header>
-
-        <Content
-          className="site-layout"
-          style={{ padding: "0 50px", marginTop: 64 }}
-        >
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>All Posts</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className={cn('create')}>
-            <h1>Welcome to Our Image Board!</h1>
-          <Button/>
-          </div>
-          
-          <div
-            className="site-layout-background"
-            style={{ padding: 24, minHeight: 380 }}
-          >
-            
-            <PostsList postsData={posts}/>
-
-          </div>
-        </Content>
-        <Footer className="footer" style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
-      </Layout>
-    </Styles>
+    <>
+      <Header />
+      <Container>
+        <div className={cn("add__margin")}>
+        <Breadcrumb/>
+        </div>
+        <div className={cn("create")}>
+          <h1>Welcome to Our Image Board!</h1>
+          <Button />
+        </div>
+        <PostsList postsData={posts} rows={rows}/>
+        <Pagination page={page} onChange={setPage}/>
+      </Container>
+      <Footer className={cn('footer')}/>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { App } from './App';
-import { AppMUI } from './AppMui';
+import { App } from './App';
+// import { AppMUI } from './AppMui';
 
-import 'antd/dist/antd.css';
 import './index.css';
 
-
-
-
-ReactDOM.render(<AppMUI/>, document.querySelector("#root"));
+ReactDOM.render(<App/>, document.querySelector("#root"));
